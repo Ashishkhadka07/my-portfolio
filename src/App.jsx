54693c9013c488cdcd55682799100fd82bb4bfd3
Ashish Khadka <aasheeshkhadka@gmail.com>
2025-12-24 15:06:45 +0545

    react vite setup with tailwind css, react-router-dom and react-icons done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
